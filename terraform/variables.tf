15c68faed0ded4ef98d11cc3c894303c15b72656
Shubhshekhar99 <155132346+Shubhshekhar99@users.noreply.github.com>
2026-01-12 02:33:33 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,44 +1,42 @@
 variable "aws_region" {
   type    = string
-  default = "us-west-1"
+  
 }
 
 variable "cluster_name" {
   type    = string
-  default = "demo-eks"
+  
 }
 
 variable "vpc_cidr" {
   type    = string
-  default = "10.0.0.0/16"
+
 }
 
 variable "public_subnet_1_cidr" {
   type    = string
-  default = "10.0.1.0/24"
 }
 
 variable "public_subnet_2_cidr" {
   type    = string
-  default = "10.0.2.0/24"
+
 }
 
 variable "node_group_desired_size" {
   type    = number
-  default = 2
 }
 
 variable "node_group_min_size" {
   type    = number
-  default = 1
+  
 }
 
 variable "node_group_max_size" {
   type    = number
-  default = 3
+  
 }
 
 variable "node_instance_type" {
   type    = string
-  default = "t3.medium"
+  
 }
