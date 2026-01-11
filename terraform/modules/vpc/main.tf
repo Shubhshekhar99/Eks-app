b58f984dc557211fe6eb5fbae36d298f1f6cc4bd
Shubhshekhar99 <155132346+Shubhshekhar99@users.noreply.github.com>
2026-01-12 00:41:27 +0530

    Create main.tf

---
+++ terraform/modules/vpc/main.tf
@@ -0,0 +1,17 @@
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr
+  enable_dns_support = true
+  enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "private1" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.private_subnet_1_cidr
+  availability_zone = "us-west-1a"
+}
+
+resource "aws_subnet" "private2" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.private_subnet_2_cidr
+  availability_zone = "us-west-1c"
+}
