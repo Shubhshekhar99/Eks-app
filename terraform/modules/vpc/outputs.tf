deee4c7cce920ebd597e0cb75d226025b9e1d1ee
Shubhshekhar99 <155132346+Shubhshekhar99@users.noreply.github.com>
2026-01-12 02:27:01 +0530

    Update outputs.tf

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -1,10 +1,12 @@
+# Outputs
 output "vpc_id" {
   value = aws_vpc.vpc.id
 }
 
-output "private_subnets" {
-  value = [
-    aws_subnet.private1.id,
-    aws_subnet.private2.id
-  ]
+output "public1_id" {
+  value = aws_subnet.public1.id
+}
+
+output "public2_id" {
+  value = aws_subnet.public2.id
 }
