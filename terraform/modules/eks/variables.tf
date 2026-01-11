54dc70044c8943658dad53cacfd0bfec125684eb
Shubhshekhar99 <155132346+Shubhshekhar99@users.noreply.github.com>
2026-01-12 02:29:36 +0530

    Update variables.tf

--- terraform/modules/eks/variables.tf
+++ terraform/modules/eks/variables.tf
@@ -1,3 +1,9 @@
 variable "cluster_name" {}
-variable "subnet_ids" {}
-variable "vpc_id" {}
+variable "subnet_ids" {
+  type = list(string)
+}
+
+variable "node_group_desired_size" {}
+variable "node_group_min_size" {}
+variable "node_group_max_size" {}
+variable "node_instance_type" {}
