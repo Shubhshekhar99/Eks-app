6639c4457052856a92d02f2086f02c02375b3776
Shubhshekhar99 <155132346+Shubhshekhar99@users.noreply.github.com>
2026-01-12 02:30:28 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,26 @@
+# -------------------------
+# VPC Module
+# -------------------------
 module "vpc" {
   source = "./modules/vpc"
 
-  vpc_cidr              = "10.0.0.0/16"
-  private_subnet_1_cidr = "10.0.1.0/24"
-  private_subnet_2_cidr = "10.0.2.0/24"
+  cluster_name        = var.cluster_name
+  vpc_cidr            = var.vpc_cidr
+  public_subnet_1_cidr = var.public_subnet_1_cidr
+  public_subnet_2_cidr = var.public_subnet_2_cidr
 }
 
+# -------------------------
+# EKS Module
+# -------------------------
 module "eks" {
-  source       = "./modules/eks"
-  cluster_name = var.cluster_name
-  subnet_ids   = module.vpc.private_subnets
-  vpc_id       = module.vpc.vpc_id
+  source = "./modules/eks"
+
+  cluster_name          = var.cluster_name
+  subnet_ids            = [module.vpc.public1_id, module.vpc.public2_id]
+
+  node_group_desired_size = var.node_group_desired_size
+  node_group_min_size     = var.node_group_min_size
+  node_group_max_size     = var.node_group_max_size
+  node_instance_type      = var.node_instance_type
 }
