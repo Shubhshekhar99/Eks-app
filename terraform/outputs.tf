5e10e1c07044f905779e0bfe3bd71fd167f6a01d
Shubhshekhar99 <155132346+Shubhshekhar99@users.noreply.github.com>
2026-01-12 02:31:21 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,15 @@
-output "cluster_name" {
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "public_subnets" {
+  value = [module.vpc.public1_id, module.vpc.public2_id]
+}
+
+output "eks_cluster_name" {
   value = module.eks.cluster_name
+}
+
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
 }
