d6e742cc751567154828e36426f46cdd95ce0281
Shubhshekhar99 <155132346+Shubhshekhar99@users.noreply.github.com>
2026-01-14 12:39:06 +0530

    Update variables.tf

--- terraform/modules/eks/variables.tf
+++ terraform/modules/eks/variables.tf
@@ -1,9 +1,27 @@
-variable "cluster_name" {}
+variable "cluster_name" {
+  type = string
+}
+
 variable "subnet_ids" {
   type = list(string)
 }
 
-variable "node_group_desired_size" {}
-variable "node_group_min_size" {}
-variable "node_group_max_size" {}
-variable "node_instance_type" {}
+variable "node_instance_type" {
+  type    = string
+  default = "t3.medium"
+}
+
+variable "node_group_desired_size" {
+  type    = number
+  default = 2
+}
+
+variable "node_group_min_size" {
+  type    = number
+  default = 2
+}
+
+variable "node_group_max_size" {
+  type    = number
+  default = 3
+}
